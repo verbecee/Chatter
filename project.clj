1fcba8544e65b02c45b6f85872dcb76dd1969ecc
verbecee <erin.e.verbeck@vanderbilt.edu>
2018-10-07 16:35:17 -0500

    add-messages

--- project.clj
+++ project.clj
@@ -1,13 +1,27 @@
-(defproject chatter "0.1.0-SNAPSHOT"
-  :description "Caffeine makes my hands shake"
-  :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]
-                 [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.12.4"]]
-  :ring {:handler chatter.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        (defproject chatter "0.1.0-SNAPSHOT"
+                          :description "clojure web app for displaying messages"
+                          :url "http://example.com/FIXME"
+                          :min-lein-version "2.0.0"
+                          :dependencies [[org.clojure/clojure "1.9.0"]
+                                         [compojure "1.6.1"]
+                                         [ring/ring-defaults "0.3.2"]
+                                         [ring/ring-jetty-adapter "1.7.0"]
+                                         [hiccup "1.0.5"]
+                                         [hickory "0.7.1"]
+                                         [environ "1.0.2"]]
+                          :plugins [[lein-ring "0.9.7"]
+                                    [lein-environ "1.0.0"]]
+                          :ring {:handler chatter.handler/app
+                                 :init chatter.handler/init
+                                 :destroy chatter.handler/destroy}
+                          :aot :all
+                          :main chatter.handler
+                          :profiles
+                          {:dev
+                           {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                           [ring/ring-mock "0.3.2"]]}
+                           :production
+                           {:ring
+                            {:open-browser? false, :stacktraces? false, :auto-reload? false}
+                            :env {production true}}}
+                          :uberjar-name "chatter-standalone.jar")
--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,19 +2,55 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.page :as page]))
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
+            [hiccup.page :as page]
+            [hiccup.form :as form]
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
+
+(def chat-messages (atom '()))
+
+(defn update-messages!
+  "This will update a message list atom"
+  [messages name message]
+  (swap! messages conj {:name name :message message}))
 
 (defn generate-message-view
   "This generates the HTML for displaying messages"
-  []
+  [messages]
     (page/html5
       [:head
         [:title "Chatter"]]
       [:body
-        [:h1 "Our Chat App"]]))
+        [:h1 "Our Chat App"]
+        [:pg
+          (form/form-to
+            [:post "/"]
+            "Name: " (form/text-field "name")
+            "Message: " (form/text-field "msg")
+            (form/submit-button "Submit"))]
+        [:p
+          [:table
+            (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
 (defroutes app-routes
-  (GET "/" [] (generate-message-view))
+  (GET "/" [] (generate-message-view @chat-messages))
+  (POST "/" {params :params}
+    (let [name-param (get params "name")
+          msg-param (get params "msg")
+          new-messages (update-messages! chat-messages name-param msg-param)]
+          (generate-message-view new-messages)
+          ))
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (wrap-params app-routes))
+
+(defn init []
+  (println "chatter is starting"))
+
+(defn destroy []
+  (println "chatter is shutting down"))
+
+(defn -main [& [port]]
+    (let [port (Integer. (or port (env :port) 5000))]
+      (jetty/run-jetty #'app {:port port :join? false})))
